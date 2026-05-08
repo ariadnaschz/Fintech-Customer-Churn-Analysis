5f8ba3f7e09298a764af5afca632ae4c4dfbe417
Ariadna Sánchez <lizzetha.ss@gmail.com>
2026-05-08 16:50:40 -0600

    feat: modificacion del scrip y exportacion de datos

--- src/generate_predictions.py
+++ src/generate_predictions.py
@@ -2,28 +2,31 @@ import pandas as pd
 import joblib
 
 def generate_dashboard_data():
-    print("1. Cargando datos y modelo...")
-    # Cargar los datos limpios
+    print("1. Cargando datos...")
+    # Cargar los datos limpios (para el modelo)
     df = pd.read_csv('data/processed/cleaned_data.csv')
     
-    # "Descongelar" el modelo y las columnas
+    # Cargar los datos crudos (solo para rescatar el ID y el Apellido)
+    raw_df = pd.read_csv('data/raw/Churn_Modelling.csv')
+    
+    # Cargar el modelo
     modelo = joblib.load('models/churn_model.pkl')
     columnas_modelo = joblib.load('models/model_columns.pkl')
     
-    # Asegurarnos de que usamos exactamente las columnas que el modelo conoce
     X = df[columnas_modelo]
     
     print("2. Generando predicciones...")
-    # Predecir si se va (1) o se queda (0)
     df['Prediccion_Fuga'] = modelo.predict(X)
-    
-    # Probabilidad de fuga
     probabilidades = modelo.predict_proba(X)
     df['Probabilidad_Fuga_%'] = (probabilidades[:, 1] * 100).round(2)
     
-    # 3. Exportar para Power BI
+    # 3. Pegar el ID y el Apellido al principio del DataFrame
+    df.insert(0, 'CustomerId', raw_df['CustomerId'])
+    df.insert(1, 'Surname', raw_df['Surname'])
+    
+    # 4. Exportar
     df.to_csv('data/processed/dashboard_data.csv', index=False)
-    print("✅ ¡Listo! Datos exportados a data/processed/dashboard_data.csv")
+    print("✅ ¡Listo! Datos exportados con CustomerId y Surname incluidos.")
 
 if __name__ == "__main__":
     generate_dashboard_data()
